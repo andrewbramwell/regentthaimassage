5f5eea0bfc69cd6cd3bf7144f436c16666d78e84
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2025-09-28 14:06:12 +0100

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "Lily and Rose Thai Massage and Retreat Centre",
+    "email": "",
+    "phoneForTel": "+447780475640",
+    "phoneFormatted": "07780 475640",
+    "openingHours": "Mon-Sun: 9am-9pm",
+    "onlineBooking": "https://lilyandrosethaimassage.setmore.com/",
+    "address": {
+        "lineOne": "Aaron Hill Farm",
+        "lineTwo": "Cragg Vale",
+        "city": "Hebden Bridge",
+        "state": "",
+        "zip": "HX7 5TX",
+        "country": "UK",
+        "mapLink": "https://maps.app.goo.gl/oMwbkapazBG1ma4d9"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+        "googleReview": "",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.example.com"
+};
