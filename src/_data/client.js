c9994f25e629068c869823319e744b3881c323ff
andrewbramwell <andy@voucherstore.io>
2025-10-07 15:50:06 +0100

    first commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,24 +1,27 @@
 module.exports = {
-    "name": "Lily and Rose Thai Massage and Retreat Centre",
-    "email": "",
-    "phoneForTel": "+447780475640",
-    "phoneFormatted": "07780 475640",
-    "openingHours": "Mon-Sun: 9am-9pm",
-    "onlineBooking": "https://lilyandrosethaimassage.setmore.com/",
+    "name": "Regent Thai Massage & Spa",
+    "email": "info@regentthaimassageandspa.co.uk",
+    "phoneForTel": "+447301291923",
+    "phoneFormatted": "07301 291923",
+    "openingHours": "Mon-Sat: 10am-8pm, Sun: 11am-7pm",
+    "onlineBooking": "https://regentthaimassagespa.setmore.com/",
     "address": {
-        "lineOne": "Aaron Hill Farm",
-        "lineTwo": "Cragg Vale",
-        "city": "Hebden Bridge",
+        "lineOne": "3 Ormond Terrace",
+        "lineTwo": "Regent St",
+        "city": "Cheltenham",
         "state": "",
-        "zip": "HX7 5TX",
+        "zip": "GL50 1HR",
         "country": "UK",
-        "mapLink": "https://maps.app.goo.gl/oMwbkapazBG1ma4d9"
+        "mapLink": "https://maps.app.goo.gl/qAHseAEmAKRpXHxF6"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-        "googleReview": "",
+        "facebook": "https://www.facebook.com/people/Regent-Thai-massage-Cheltenham/61570157464844/?locale=en_GB",
+        "instagram": "https://www.instagram.com/regent_thaimassage_cheltenham/",
+        "googleReview": "https://g.page/r/CY4v3czmGv6ZEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.regentthaimassageandspa.co.uk"
 };
+
+
+
